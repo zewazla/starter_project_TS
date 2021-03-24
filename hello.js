840b868ce91209285ed6bd6a67ae2199db96bb79
Bożka Majchrzak <zewazla@gmail.com>
2021-03-24 12:38:08 +0100

    starter project TS

---
+++ hello.js
@@ -0,0 +1,5 @@
+"use strict";
+var firstName = "John";
+var lastName = "Joplin";
+var age = 38;
+console.log(firstName, lastName, age);
---
+++ hello.ts
@@ -0,0 +1,5 @@
+const firstName: string = "John";
+const lastName: string = "Joplin";
+const age: number = 38;
+
+console.log(firstName, lastName, age);
